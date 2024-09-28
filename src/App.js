4c337ba9fb8806f9cfd2392b19ca8b5aeaeba794
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-09-28 18:05:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("")
+  const [result, setResult] = useState("")
+
+  const handleCalculateResult = () => {
+    try {
+      let result = new Function(`return ${inputValue}`)();
+      return setResult(result);
+    } catch (error) {
+      return setResult("Error");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+
+      <input type='text' value={inputValue} />
+      <div>{result}</div>
+      <div><button onClick={() => { setInputValue(inputValue + "1") }}>1</button>
+        <button onClick={() => { setInputValue(inputValue + "2") }}>2</button>
+        <button onClick={() => { setInputValue(inputValue + "3") }}>3</button>
+        <button onClick={() => { setInputValue(inputValue + "+") }}>+</button></div>
+      <div><button onClick={() => { setInputValue(inputValue + "4") }}>4</button>
+        <button onClick={() => { setInputValue(inputValue + "5") }}>5</button>
+        <button onClick={() => { setInputValue(inputValue + "6") }}>6</button>
+        <button onClick={() => { setInputValue(inputValue + "-") }}>-</button></div>
+      <div><button onClick={() => { setInputValue(inputValue + "7") }}>7</button>
+        <button onClick={() => { setInputValue(inputValue + "8") }}>8</button>
+        <button onClick={() => { setInputValue(inputValue + "9") }}>9</button>
+        <button onClick={() => { setInputValue(inputValue + "*") }}>*</button></div>
+      <div><button onClick={() => {
+        setInputValue("")
+        setResult("")
+      }}>C</button>
+        <button onClick={() => { setInputValue(inputValue + "0") }}>0</button>
+        <button onClick={handleCalculateResult}>=</button>
+        <button onClick={() => { setInputValue(inputValue + "/") }}>/</button></div>
     </div>
   );
 }
